78fa48def1fdde70eeb14923735e88e0f7bb16c4
jackson <s97403002a@gmail.com>
2022-05-17 21:33:20 +0800

    add Try Catch

---
+++ TryCatchDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace TryCatchDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("input :");
+            var input = 0;
+            try
+            {
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine(input);
+            
+        }
+    }
+}
