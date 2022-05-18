de5b40891660af850daed0dcabf978d67bccf800
jackson <s97403002a@gmail.com>
2022-05-18 22:04:39 +0800

    add ifelse for Demo

---
+++ ForDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ForDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Q1();
+            Q2();
+           Q3();
+           Q4();
+             Q5();
+             Q6();
+        }
+
+        static void Q1()
+        {
+            DisplayHeader("三角形");
+            int rows = 5;
+
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Q2()
+        {
+            DisplayHeader("倒三角形");
+            int rows = 5;
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j <= 5 - i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Q3()
+        {
+            DisplayHeader("靠右對齊三角形");
+            int rows = 5;
+            for(int i = 0; i <= rows; i++)
+            {
+                for(int j = 5; j >= 0; j--)
+                {
+                    if (i < j)
+                    {   Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                 
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        static void Q4()
+        {
+            DisplayHeader("靠右對齊倒三角形");
+            int rows = 5;
+            for (int i = rows; i >0; i--)
+            {
+                string row = new string(' ', rows - i) + new string('*', i);
+                Console.WriteLine(row);
+            }
+        }
+
+        static void Q5()
+        {
+            DisplayHeader("等腰三角形");
+            
+
+            int rows = 4;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                string row = new string(' ', rows - i) + new string('*', i*2-1);
+                Console.WriteLine(row);
+            }
+        }
+
+        static void Q6()
+        {
+            DisplayHeader("倒等腰三角形");
+            int rows = 4;
+
+            for (int i = rows; i >0; i--)
+            {
+                string row = new string(' ', rows - i) + new string('*', i * 2 - 1);
+                Console.WriteLine(row);
+            }
+        }
+
+        static void DisplayHeader(string title)
+        {
+            Console.WriteLine("\r\n");
+            Console.WriteLine(title);
+            Console.WriteLine(new string('=', 40));
+        }
+    }
+}
---
+++ IfElseDemo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace IfElseDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // Q04010101();
+            Q1(DateTime.Now);
+            Q2(DateTime.Now);
+            
+        }
+
+        private static void Q04010101()
+        {
+            // 計算一天停車費, 一小時5 元, 每天最多收 30元
+            Console.Write("請輸入停車的小時數(1~24):");
+            var input = Console.ReadLine();
+
+            // 在本練習裡,不特別針對輸入值是否正確進行檢查
+            var hours = int.Parse(input);
+
+            var fee = CalculateParkingRates(hours);
+
+            Console.WriteLine($"停車時數={hours}, 停車費用為 {fee}");
+        }
+        private static void Q1(DateTime dt)
+        {
+            string greeting = string.Empty;
+            if (dt < dt.Date.AddHours(12))
+            {
+                greeting = "早安";
+            }else if (dt < dt.Date.AddHours(18))
+            {
+                greeting = "午安";
+            }
+            else
+            {
+                greeting = "晚安";
+            }
+
+            Console.WriteLine(greeting);
+        }
+
+        /// <summary>
+        /// 根據目前時間, 決定顯示什麼問安文字
+        /// - 大於或等於 00:00 且小於 11:30 : 早安
+        /// - 大於或等於 11:30 且小於 17:45 : 午安
+        /// - 大於或等於 17:45: 晚安
+        /// </summary>
+        private static void Q2(DateTime dt)
+        {
+            string greeting = string.Empty;
+            if (dt < dt.Date.AddHours(11.5))
+            {
+                greeting = "早安";
+            }
+            else if (dt < dt.Date.AddHours(17).AddMinutes(45))
+            {
+                greeting = "午安";
+            }
+            else
+            {
+                greeting = "晚安";
+            }
+
+            Console.WriteLine(greeting);
+        }
+        private static int CalculateParkingRates(int hours)
+        {
+            var price = hours * 5;
+            return price > 30 ? 30 : price;
+        }
+    }
+}
