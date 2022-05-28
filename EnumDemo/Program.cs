1331b70ad17fd5bd256eb54a21e83da0bf3fccfe
jackson <s97403002a@gmail.com>
2022-05-28 23:54:30 +0800

    add enum

---
+++ EnumDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    public enum Gander
+    {
+        男,
+        女
+    }
+
+    public enum Seson
+    {
+        春,
+        夏,
+        秋,
+        冬
+    }
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var gander = Gander.男;
+            Console.WriteLine((int)gander);
+        }
+    }
+}
